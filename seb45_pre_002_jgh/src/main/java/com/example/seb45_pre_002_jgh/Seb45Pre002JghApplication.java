7b1b147f55f31e07e41eb2788d6239bb13622f89
JangGH <jgh3998@gmail.com>
2023-08-07 16:44:56 +0900

    Initial commit

---
+++ seb45_pre_002_jgh/src/main/java/com/example/seb45_pre_002_jgh/Seb45Pre002JghApplication.java
@@ -0,0 +1,13 @@
+package com.example.seb45_pre_002_jgh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seb45Pre002JghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seb45Pre002JghApplication.class, args);
+	}
+
+}
---
+++ seb45_pre_002_jgh/src/test/java/com/example/seb45_pre_002_jgh/Seb45Pre002JghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seb45_pre_002_jgh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seb45Pre002JghApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
